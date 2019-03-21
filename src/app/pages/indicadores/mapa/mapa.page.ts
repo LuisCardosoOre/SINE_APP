ebe066eb46e36aacb9e9f8a321bd4e86e2c49071
LuisCardosoOre <Luis_93_13@hotmail.com>
2019-03-21 18:30:07 -0500

    Mapa clic

--- src/app/pages/indicadores/mapa/mapa.page.ts
+++ src/app/pages/indicadores/mapa/mapa.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mapa',
@@ -6,17 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapa.page.scss'],
 })
 export class MapaPage implements OnInit {
-
+  constructor(public router: Router, private actionSheetCtrl: ActionSheetController) { }
 
   Nombre_Red: string;
 
-
-  constructor() { }
-
   ngOnInit() {
   }
-
-
 
   async onClick(event: any) {
 
@@ -25,4 +22,45 @@ export class MapaPage implements OnInit {
 
   }
 
+  async CambioPagina() {
+
+    return this.router.navigateByUrl('/lista-indicadores').then (e => {
+      if (this.Nombre_Red = 'Sabogal') {
+        console.log(this.Nombre_Red);
+      }
+    });
+
+  }
+
+  async ListaRedesLima(event: any) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Redes de Lima',
+      buttons: [{
+        text: 'Red Almenara',
+        icon: 'checkmark-circle',
+        cssClass: 'azul',
+        handler: () => {
+          this.Nombre_Red = 'Almenara';
+
+        }
+      }, {
+        text: 'Red Rebagliati',
+        icon: 'checkmark-circle',
+        cssClass: 'azul',
+        handler: () => {
+          this.Nombre_Red = 'Rebagliati';
+
+        }
+      }, {
+        text: 'Red Sabogal',
+        icon: 'checkmark-circle',
+        cssClass: 'azul',
+        handler: () => {
+          this.Nombre_Red = 'Sabogal';
+
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
